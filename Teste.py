8cc4a5e3e6005c9e8a2e68f388360eeb063bb6de
thaylanne <thaylanne.beserra@gmail.com>
2022-09-21 11:33:42 -0300

    Incluído estrutura condicional e resultado final

--- Teste.py
+++ Teste.py
@@ -4,6 +4,7 @@ valorLiberado : float
 taxaJuros : int
 ValorSolicitado : float
 Parcelas : int
+ValorFinal : float
 
 salario = float(input("Digite sua renda: "))
 score = int(input("Digite seu Score: "))
@@ -11,9 +12,27 @@ valorLiberado = salario * 35/100 * 12
 
 print(f"Renda: {salario:.2f}")
 print(f"Score: {score}")
-print(f"Valor Máximo Liberado de Empréstimo: {valorLiberado:.2f}")
+print("Informações:")
+print(f"Valor Total a ser disponibilizado: {valorLiberado:.2f}")
 ValorSolicitado = int(input("Digite valor que deseja de Empréstimo: "))
-Parcelas = int(input("Digite em quantas parcelas deseja pagar 1 até 12: "))
+Parcelas = int(input("Digite em quantas parcelas deseja pagar *1 até 12*: "))
+
+if score <= 533:
+    taxaJuros = 9
+elif score <= 766:
+    taxaJuros = 7
+else:
+    taxaJuros = 3
+
+ValorFinal = ValorSolicitado * (1 + taxaJuros/100) / Parcelas
+
+print(f"Taxa de Juros: {taxaJuros} %")
+print(f"{Parcelas} parcelas mensais de: R$ {ValorFinal:.2f}")
+
+
+
+
+
 
 
 
